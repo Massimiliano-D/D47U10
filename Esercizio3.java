ef7e19b1c0aed1e3226579b024c681f83744f831
Massimiliano-D <137884771+Massimiliano-D@users.noreply.github.com>
2023-10-03 16:02:57 +0200

    es N3
    fatto senza Scanner

---
+++ Esercizio3.java
@@ -0,0 +1,10 @@
+public class Esercizio3 {
+    public static void main(String[] args) {
+        int numeroIniziale = 10;
+        System.out.println("Conto alla rovescia");
+        while (numeroIniziale >= 0) {
+            System.out.println(numeroIniziale);
+            numeroIniziale--;
+        }
+    }
+}
